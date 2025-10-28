4187610c584d7249421bb1563fdc9fed59edaf7f
blofl <bob.lofland@gmail.com>
2025-10-27 20:43:24 -0700

    Update message in main branch

--- src/app.py
+++ src/app.py
@@ -2,4 +2,5 @@ print("Hello from Python!")
 print("This is my first project")
 print("And this is another update")
 print("This is my first project - from feature branch")
+print("this is the version from the main branch")
 

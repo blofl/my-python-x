f2c646e7d0f71daccefc6dd24539c0efc4d108c6
blofl <bob.lofland@gmail.com>
2025-10-28 21:17:35 -0700

    Feature: update message from PR branch

--- src/app.py
+++ src/app.py
@@ -3,4 +3,5 @@ print("This is my first project")
 print("And this is another update")
 print("This is my first project - from feature branch")
 print("this is the version from the main branch")
+print("this is the version from the PR feature branch")
 
